6e37769009a100eab4ec293f0856c5dde49c66f1
wlukla <vladislav.danilochkin@gmail.com>
2019-11-11 17:26:10 +0200

    refactor: change script according to config-airbnb-base

--- palette/script.js
+++ palette/script.js
@@ -2,15 +2,16 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const colorSwitcher = document.querySelector('.color__icon_current');
 const prevColor = document.querySelector('.color__icon_prev');
-const red = document.querySelector('.color__icon_red')
-const blue = document.querySelector('.color__icon_blue')
+const red = document.querySelector('.color__icon_red');
+const blue = document.querySelector('.color__icon_blue');
 let lastColor = colorSwitcher.value;
 
+
 function buildCanvas(size, color = '#D3D3D3') {
-  let map = [];
-  for (let i = 0; i < size; i++) {
+  const map = [];
+  for (let i = 0; i < size; i += 1) {
     map.push([]);
-    for (let j = 0; j < size; j++) {
+    for (let j = 0; j < size; j += 1) {
       map[i][j] = color;
     }
   }
@@ -26,34 +27,27 @@ if (window.localStorage.getItem('map') === null) {
   map = _.chunk(map, 4);
 }
 
-drawHex(map, 512 / map.length);
+function drawHex(imageArr, pixelSize) {
+  for (let i = 0; i < imageArr.length; i += 1) {
+    for (let j = 0; j < imageArr[i].length; j += 1) {
+      ctx.fillStyle = imageArr[i][j];
+      ctx.fillRect(i * pixelSize, j * pixelSize, pixelSize, pixelSize);
+    }
+  }
+}
 
-const size4Button = document.getElementById('size4');
-const size32Button = document.getElementById('size32');
-const size256Button = document.getElementById('size256');
-const sizeButtons = document.querySelectorAll('.sheet__size-switcher');
+drawHex(map, 512 / map.length);
 
 const toolButtons = document.querySelectorAll('.sheet__tool');
 const fill = toolButtons[0];
 const pick = toolButtons[1];
 const pencil = toolButtons[2];
 
-function drawHex(imageArr, pixelSize) {
-  for (let i = 0; i < imageArr.length; i++) {
-    for (let j = 0; j < imageArr[i].length; j++) {
-      ctx.fillStyle = imageArr[i][j];
-      ctx.fillRect(i * pixelSize, j * pixelSize, pixelSize, pixelSize)
-    }
-  }
-}
-
 let instrument = null;
 let isDrawing = false;
-let pixelSize = 512 / 4;
-let color = colorSwitcher.value;
+const pixelSize = 512 / 4;
 
 document.addEventListener('click', (e) => {
-  color = colorSwitcher.value;
   if (e.path.includes(pencil)) {
     instrument = 2;
     toolButtons.forEach((el) => el.classList.remove('sheet__tool_selected'));
@@ -67,26 +61,26 @@ document.addEventListener('click', (e) => {
     toolButtons.forEach((el) => el.classList.remove('sheet__tool_selected'));
     pick.classList.add('sheet__tool_selected');
   }
-})
+});
 
 canvas.addEventListener('click', (e) => {
   // imlement fill
   if (instrument === 0) {
-    map = buildCanvas(4, color);
+    map = buildCanvas(4, colorSwitcher.value);
     drawHex(map, 512 / map.length);
   }
 
   // implement color picker
   if (instrument === 1) {
-    let x = Math.floor(e.offsetX / pixelSize);
-    let y = Math.floor(e.offsetY / pixelSize);
+    const x = Math.floor(e.offsetX / pixelSize);
+    const y = Math.floor(e.offsetY / pixelSize);
     colorSwitcher.value = map[x][y];
     prevColor.value = lastColor;
     lastColor = colorSwitcher.value;
   }
 
   window.localStorage.setItem('map', map);
-})
+});
 
 document.addEventListener('click', (e) => {
   if (e.target === red) {
@@ -100,34 +94,34 @@ document.addEventListener('click', (e) => {
     prevColor.value = colorSwitcher.value;
     colorSwitcher.value = blue.value;
   }
-})
+});
 
 canvas.addEventListener('mousedown', (e) => {
   isDrawing = true;
   if (instrument === 2) {
-    let x = Math.floor(e.offsetX / pixelSize);
-    let y = Math.floor(e.offsetY / pixelSize);
-    map[x][y] = color;
+    const x = Math.floor(e.offsetX / pixelSize);
+    const y = Math.floor(e.offsetY / pixelSize);
+    map[x][y] = colorSwitcher.value;
     drawHex(map, 512 / map.length);
   }
-})
+});
 
 canvas.addEventListener('mouseout', () => {
   isDrawing = false;
-})
+});
 
 canvas.addEventListener('mousemove', (e) => {
   if (instrument === 2 && isDrawing) {
-    let x = Math.floor(e.offsetX / pixelSize);
-    let y = Math.floor(e.offsetY / pixelSize);
-    map[x][y] = color;
+    const x = Math.floor(e.offsetX / pixelSize);
+    const y = Math.floor(e.offsetY / pixelSize);
+    map[x][y] = colorSwitcher.value;
     drawHex(map, 512 / map.length);
   }
-})
+});
 
 canvas.addEventListener('mouseup', () => {
   isDrawing = false;
-})
+});
 
 document.addEventListener('keydown', (e) => {
   if (e.code === 'KeyB') {
@@ -143,9 +137,23 @@ document.addEventListener('keydown', (e) => {
     toolButtons.forEach((el) => el.classList.remove('sheet__tool_selected'));
     pick.classList.add('sheet__tool_selected');
   }
-})
+});
 
 colorSwitcher.addEventListener('change', () => {
   prevColor.value = lastColor;
   lastColor = colorSwitcher.value;
-})
+});
+
+// function defineArea(map, color) {
+//   let area = [];
+//   for (let i = 0; i < map.length; i++) {
+//     for (let j = 0; j < map[i].length; j++) {
+//       if (map[i][j] == color) {
+//         area.push([i, j]);
+//       }
+//     }
+//   }
+//   console.log(area);
+// }
+
+// defineArea(map, '#f74141');
