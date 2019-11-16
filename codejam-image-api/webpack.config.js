9f37c7ba112f9bddd2d1494df7c3eceec10c9a2c
wlukla <vladislav.danilochkin@gmail.com>
2019-11-16 13:53:14 +0200

    feat: add dev-server

--- codejam-image-api/webpack.config.js
+++ codejam-image-api/webpack.config.js
@@ -2,12 +2,18 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry:{
     app: './src/index.js',
   },
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
   },
   module: {
     rules: [
