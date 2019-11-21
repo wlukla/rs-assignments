7b99ea085c05aea589a996935e14cde5560be09f
wlukla <vladislav.danilochkin@gmail.com>
2019-11-21 20:19:21 +0200

    feat: add changes from image-api

--- palette/src/script.js
+++ palette/src/script.js
@@ -1,136 +1,166 @@
-import 'lodash';
 import './style.scss';
+import 'normalize.css'
+import * as Netlify from './netlify';
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+let ctxScale = 1;
+let data = '';
+
 const colorSwitcher = document.querySelector('.color__icon_current');
 const prevColor = document.querySelector('.color__icon_prev');
 const red = document.querySelector('.color__icon_red');
 const blue = document.querySelector('.color__icon_blue');
 let lastColor = colorSwitcher.value;
 
-
-function buildCanvas(size, color = '#D3D3D3') {
-  const map = [];
-  for (let i = 0; i < size; i += 1) {
-    map.push([]);
-    for (let j = 0; j < size; j += 1) {
-      map[i][j] = color;
-    }
-  }
-  return map;
-}
-
-let map = [];
-map = buildCanvas(4);
-if (window.localStorage.getItem('map') === null) {
-  map = buildCanvas(4);
-} else {
-  map = window.localStorage.getItem('map').split(',');
-  map = _.chunk(map, 4);
-}
-
-function drawHex(imageArr, pixelSize) {
-  for (let i = 0; i < imageArr.length; i += 1) {
-    for (let j = 0; j < imageArr[i].length; j += 1) {
-      ctx.fillStyle = imageArr[i][j];
-      ctx.fillRect(i * pixelSize, j * pixelSize, pixelSize, pixelSize);
-    }
-  }
-}
-
-drawHex(map, 512 / map.length);
-
 const toolButtons = document.querySelectorAll('.sheet__tool');
-const fill = toolButtons[0];
-const pick = toolButtons[1];
-const pencil = toolButtons[2];
-
+const [fill, pick, pencil] = toolButtons;
 let instrument = null;
 let isDrawing = false;
-const pixelSize = 512 / 4;
+
+const input = document.querySelector('.form__text-input');
+
+const sizeButtons = document.querySelectorAll('.sheet__size-switcher');
+const [size128Button, size256Button, size512Button] = sizeButtons;
+
+// disable smoothing
+ctx.msImageSmoothingEnabled = false;
+ctx.mozImageSmoothingEnabled = false;
+ctx.webkitImageSmoothingEnabled = false;
+ctx.imageSmoothingEnabled = false;
+
+function changeScale(i) {
+  ctxScale = i;
+  window.localStorage.setItem('scale', ctxScale);
+}
+
+function makeActive(btn) {
+  console.log(btn.classList);
+  if (btn.classList.contains('size-switcher')) {
+    sizeButtons.forEach((button) => button.classList.remove('sheet__size-switcher_active'));
+    btn.classList.add('sheet__size-switcher_active');
+  } else {
+    toolButtons.forEach((button) => button.classList.remove('sheet__tool_selected'));
+    btn.classList.add('sheet__tool_selected');
+  }
+}
+
+window.onload = () => {
+  instrument = 2;
+  toolButtons.forEach((el) => el.classList.remove('sheet__tool_selected'));
+  pencil.classList.add('sheet__tool_selected');
+
+  // updating everything from storage
+  if (localStorage.getItem('data') !== null) {
+    hasImage = true;
+    // setting last color
+    colorSwitcher.value = window.localStorage.getItem('color');
+
+    // setting link to image
+    data = window.localStorage.getItem('lastImg');
+
+    // drawing canvas
+    const oldImg = new Image();
+    oldImg.src = localStorage.getItem('data');
+    oldImg.onload = async () => {
+      ctxScale = +localStorage.getItem('scale');
+      ctx.drawImage(oldImg, 0, 0);
+      // highliting last canvas size
+      if (ctxScale === 1) {
+        makeActive(size512Button);
+      } else if (ctxScale === 2) {
+        makeActive(size256Button);
+      } else if (ctxScale === 4) {
+        makeActive(size128Button);
+      }
+    };
+  } else {
+    // seting canvas size to 128x128
+    changeScale(4);
+  }
+};
+
+function saveCtx() {
+  window.localStorage.setItem('data', canvas.toDataURL());
+  window.localStorage.setItem('color', colorSwitcher.value);
+}
 
 document.addEventListener('click', (e) => {
+  // rewrite with make active
+
   if (e.path.includes(pencil)) {
     instrument = 2;
-    toolButtons.forEach((el) => el.classList.remove('sheet__tool_selected'));
-    pencil.classList.add('sheet__tool_selected');
+    makeActive(pencil);
   } else if (e.path.includes(fill)) {
     instrument = 0;
-    toolButtons.forEach((el) => el.classList.remove('sheet__tool_selected'));
-    fill.classList.add('sheet__tool_selected');
+    makeActive(fill);
   } else if (e.path.includes(pick)) {
     instrument = 1;
-    toolButtons.forEach((el) => el.classList.remove('sheet__tool_selected'));
-    pick.classList.add('sheet__tool_selected');
+    makeActive(pick);
   }
 });
 
+function drawPixel(e) {
+  const startX = Math.floor(e.offsetX / ctxScale) * ctxScale;
+  const startY = Math.floor(e.offsetY / ctxScale) * ctxScale;
+  ctx.fillStyle = colorSwitcher.value;
+  ctx.fillRect(startX, startY, ctxScale, ctxScale);
+}
+
 canvas.addEventListener('click', (e) => {
-  // imlement fill
-  if (instrument === 0) {
-    map = buildCanvas(4, colorSwitcher.value);
-    drawHex(map, 512 / map.length);
+  if (instrument === 2) {
+    drawPixel(e);
   }
-
-  // implement color picker
-  if (instrument === 1) {
-    const x = Math.floor(e.offsetX / pixelSize);
-    const y = Math.floor(e.offsetY / pixelSize);
-    colorSwitcher.value = map[x][y];
-    prevColor.value = lastColor;
-    lastColor = colorSwitcher.value;
-  }
-
-  window.localStorage.setItem('map', map);
+  saveCtx();
 });
 
 document.addEventListener('click', (e) => {
+  // change current color to red
   if (e.target === red) {
     lastColor = red.value;
     prevColor.value = colorSwitcher.value;
     colorSwitcher.value = red.value;
   }
-
+  // change current color to blue
   if (e.target === blue) {
     lastColor = blue.value;
     prevColor.value = colorSwitcher.value;
     colorSwitcher.value = blue.value;
   }
-
+  // change current color to previous color
   if (e.target === prevColor) {
     [prevColor.value, colorSwitcher.value] = [colorSwitcher.value, prevColor.value];
   }
 });
 
 canvas.addEventListener('mousedown', (e) => {
-  isDrawing = true;
   if (instrument === 2) {
-    const x = Math.floor(e.offsetX / pixelSize);
-    const y = Math.floor(e.offsetY / pixelSize);
-    map[x][y] = colorSwitcher.value;
-    drawHex(map, 512 / map.length);
+    isDrawing = true;
+    drawPixel(e);
   }
 });
 
 canvas.addEventListener('mouseout', () => {
-  isDrawing = false;
+  if (instrument === 2) {
+    isDrawing = false;
+  }
+  saveCtx();
 });
 
 canvas.addEventListener('mousemove', (e) => {
   if (instrument === 2 && isDrawing) {
-    const x = Math.floor(e.offsetX / pixelSize);
-    const y = Math.floor(e.offsetY / pixelSize);
-    map[x][y] = colorSwitcher.value;
-    drawHex(map, 512 / map.length);
+    drawPixel(e);
   }
 });
 
 canvas.addEventListener('mouseup', () => {
-  isDrawing = false;
+  if (instrument === 2) {
+    isDrawing = false;
+  }
 });
 
 document.addEventListener('keydown', (e) => {
+  // change instrument with keys
   if (e.code === 'KeyB') {
     instrument = 0;
     toolButtons.forEach((el) => el.classList.remove('sheet__tool_selected'));
@@ -147,6 +177,164 @@ document.addEventListener('keydown', (e) => {
 });
 
 colorSwitcher.addEventListener('change', () => {
+  // update previous color if current changed
   prevColor.value = lastColor;
   lastColor = colorSwitcher.value;
+  window.localStorage.setItem('color', lastColor);
+});
+
+// unsplash.com access key
+const key = '234ecb2a20225f9a826c1c7d1f299dad56d2b0c182718bfbcbe53102ab3b481b';
+
+function getLinkToImage() {
+  const city = input.value;
+  let url = ['https://api.unsplash.com/photos/random?=&client_id=', key].join('');
+
+  if (city) {
+    // update request if city is defined
+    url = ['https://api.unsplash.com/photos/random?query=town,', city, '&client_id=', key].join('');
+  }
+
+  async function getLink(req) {
+    const response = await fetch(req);
+    const json = await response.json();
+    const link = await json.urls.small;
+    return link;
+  }
+
+  return getLink(url);
+}
+
+let hasImage = false;
+
+function addImage(img) {
+  const pixelSize = ctxScale;
+  let startX = 0;
+  let startY = 0;
+  const canvasSize = 512;
+  let ws = 0;
+  let hs = 0;
+
+  // resize image to fill whole canvas
+  if (img.width > img.height) {
+    hs = (img.height / img.width) * canvasSize;
+    ws = canvasSize;
+    startY = (canvasSize - hs) / 2;
+  } else if (img.height > img.width) {
+    ws = (img.width / img.height) * canvasSize;
+    hs = canvasSize;
+    startX = (canvasSize - ws) / 2;
+  } else {
+    ws = canvasSize / pixelSize;
+    hs = canvasSize / pixelSize;
+  }
+
+  const w = ws / pixelSize;
+  const h = hs / pixelSize;
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // draw small image on canvas
+  ctx.drawImage(img, startX, startY, w, h);
+  // store canvas
+  const smallImg = new Image();
+  smallImg.src = canvas.toDataURL();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  smallImg.onload = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // draw small image on whole canvas
+    ctx.drawImage(smallImg, startX, startY, w, h, startX, startY, ws, hs);
+    saveCtx();
+    hasImage = true;
+  };
+}
+
+function displayImage(link) {
+  window.localStorage.setItem('lastImg', link);
+
+  const img = new Image();
+
+  img.onload = () => {
+    addImage(img);
+    saveCtx();
+  };
+
+  img.crossOrigin = 'anonymous';
+  img.src = link;
+}
+
+const loadButton = document.querySelector('.form__load');
+
+loadButton.addEventListener('click', async () => {
+  data = await getLinkToImage();
+  displayImage(data);
+});
+
+// greyscale
+function toGrey() {
+  const [width, height] = [canvas.width, canvas.height];
+  // getting pixels array from canvas
+  const imgPixels = ctx.getImageData(0, 0, width, height);
+
+  for (let y = 0; y < height; y += 1) {
+    for (let x = 0; x < width; x += 1) {
+      const i = (y * 4) * width + x * 4;
+      const avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
+      imgPixels.data[i] = avg;
+      imgPixels.data[i + 1] = avg;
+      imgPixels.data[i + 2] = avg;
+    }
+  }
+
+  ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
+}
+
+
+const BWButton = document.querySelector('.form__bw');
+BWButton.addEventListener('click', () => {
+  if (hasImage) {
+    toGrey();
+  } else {
+    alert('Load image first');
+  }
+});
+
+size128Button.addEventListener('click', () => {
+  makeActive(size128Button);
+  changeScale(4);
+  displayImage(data);
+  saveCtx();
+});
+
+size256Button.addEventListener('click', () => {
+  makeActive(size256Button);
+  changeScale(2);
+  displayImage(data);
+  saveCtx();
+});
+
+size512Button.addEventListener('click', () => {
+  makeActive(size512Button);
+  changeScale(1);
+  displayImage(data);
+  saveCtx();
+});
+
+// prevent form from reloading
+const form = document.querySelector('.form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
+
+// github authorization
+const loginBtn = document.querySelector('.menu__login');
+loginBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const authenticator = new Netlify.Default({});
+  authenticator.authenticate({ provider: 'github', scope: 'user' }, (err, d) => {
+    if (err) {
+      alert(['Error Authenticating with GitHub: ', err].join(''));
+    } else {
+      alert(['Authenticated with GitHub. Access Token: ', d.token].join(''));
+    }
+  });
 });
